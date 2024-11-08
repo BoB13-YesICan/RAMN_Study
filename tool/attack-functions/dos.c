d83434fcdd22af944afeeae5a58afeae12a38690
ChoChiMoo <eddyojh@gmail.com>
2024-11-08 17:38:48 +0900

    [chore]: fix typo in dos.c

--- tool/attack-functions/dos.c
+++ tool/attack-functions/dos.c
@@ -11,7 +11,7 @@ functions:
 
 void dos_dos(int socket, struct sockaddr_can *addr, int canid, int time_diff){
     do {
-        send_can_packet(socket, addr, msq.payload1, msq.payload1_len, canid);
+        send_can_packet(socket, addr, dos.payload1, dos.payload1_len, canid);
         if (time_diff == 0) break;
     } while (1);
 }
